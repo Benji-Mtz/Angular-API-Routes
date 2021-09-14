3aaa46b6dfb4aa8ed35781475fd5834ab5e610bd
Benji-Mtz <benjim.flores@outlook.com>
2021-09-14 01:01:42 -0500

    Definición de rutas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+//Mis Modulos para la app
+import { SharedModule } from './shared/shared.module';
+import { PaisModule } from './pais/pais.module';
+// Modulo de rutas
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    SharedModule,
+    PaisModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { PaisService } from '../../services/pais.service';
+
+@Component({
+  selector: 'app-por-pais',
+  templateUrl: './por-pais.component.html',
+  styles: [
+  ]
+})
+export class PorPaisComponent {
+
+  termino: string = '';
+  hayError: boolean = false;
+
+  constructor(private paisService: PaisService) { }
+
+  buscar(){
+    this.hayError = false;
+    console.log(this.termino);
+    
+    // Se agrega el subscribe para disparar el observable
+    this.paisService.buscarPais( this.termino )
+    .subscribe( (resp) => {
+        console.log(resp);
+      }, (err) => {
+        this.hayError = true;
+      });
+  }
+
+}
